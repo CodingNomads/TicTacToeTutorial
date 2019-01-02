dd5128833d0ea3104ca0fe50f657ac3e1ecd7df4
Roi Otero Morales <roioteromorales@gmail.com>
2019-01-02 12:41:08 +0100

    Initial commit - Hello World

---
+++ app/src/main/java/co/codingnomads/tictactoetutorial/GameActivity.java
@@ -0,0 +1,13 @@
+package co.codingnomads.tictactoetutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
